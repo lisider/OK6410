d0d23d87388bf863cbe140cf9c03c683d16477d2
lisider <1368317399@qq.com>
2021-03-05 17:04:05 +0800

    修改lcd控制器配置,现在可画全屏

--- lcd/lcd.c
+++ lcd/lcd.c
@@ -1,4 +1,9 @@
-/*gpio set*/
+////////////////////////////////////////////////////////////////////////////////
+////// 						S3C6410侧 寄存器
+////////////////////////////////////////////////////////////////////////////////
+
+
+/* gpio controller */
 #define GPECON  (*((volatile unsigned long *)0x7F008080))
 #define GPEDAT  (*((volatile unsigned long *)0x7F008084))
 #define GPFCON  (*((volatile unsigned long *)0x7F0080A0))
@@ -6,39 +11,61 @@
 #define GPICON  (*((volatile unsigned long *)0x7F008100))
 #define GPJCON  (*((volatile unsigned long *)0x7F008120))
 
+// GPACONSLP 0x7F00800C R/W Port A Sleep mode Configuration Register 0x0
+#define MIFPCON     	(*((volatile unsigned long *)0x7410800C))
+// SPCON 0x7F0081A0 R/W Special Port Configuration Register 0xBFC11500
+#define SPCON         	(*((volatile unsigned long *)0x7F0081A0))
+
 /* display controller */
-#define MIFPCON     (*((volatile unsigned long *)0x7410800C))
-#define SPCON         (*((volatile unsigned long *)0x7F0081A0))
-#define VIDCON0      (*((volatile unsigned long *)0x77100000))
-#define VIDCON1      (*((volatile unsigned long *)0x77100004))
-#define VIDTCON0     (*((volatile unsigned long *)0x77100010))
-#define VIDTCON1     (*((volatile unsigned long *)0x77100014))
-#define VIDTCON2     (*((volatile unsigned long *)0x77100018))
-#define WINCON0      (*((volatile unsigned long *)0x77100020))
-#define VIDOSD0A      (*((volatile unsigned long *)0x77100040))
-#define VIDOSD0B      (*((volatile unsigned long *)0x77100044))
-#define VIDOSD0C      (*((volatile unsigned long *)0x77100048))
-#define VIDW00ADD0B0      (*((volatile unsigned long *)0x771000A0))
-#define VIDW00ADD1B0      (*((volatile unsigned long *)0x771000D0))
+#define VIDCON0      	(*((volatile unsigned long *)0x77100000))
+#define VIDCON1      	(*((volatile unsigned long *)0x77100004))
+#define VIDTCON0     	(*((volatile unsigned long *)0x77100010))
+#define VIDTCON1     	(*((volatile unsigned long *)0x77100014))
+#define VIDTCON2     	(*((volatile unsigned long *)0x77100018))
+#define WINCON0      	(*((volatile unsigned long *)0x77100020))
+#define VIDOSD0A      	(*((volatile unsigned long *)0x77100040))
+#define VIDOSD0B      	(*((volatile unsigned long *)0x77100044))
+#define VIDOSD0C      	(*((volatile unsigned long *)0x77100048))
+#define VIDW00ADD0B0   	(*((volatile unsigned long *)0x771000A0))
+#define VIDW00ADD1B0  	(*((volatile unsigned long *)0x771000D0))
 #define VIDW00ADD2      (*((volatile unsigned long *)0x77100100))
 
 
-#define  VSPW          4
-#define  VBPD         23
-#define  LINEVAL     599
-#define  VFPD          1
+////////////////////////////////////////////////////////////////////////////////
+////// 						LCD 侧 :FIT-LCD4.3
+////////////////////////////////////////////////////////////////////////////////
+
+#define RGB 1
+
+#define Horizontal_resolution 	480 // 水平
+#define Vertical_resolution 	272 // 竖直
+#define BitsPerPixel 			24
+#define DATA_INTERFACE_MODE 	RGB
 
 
-#define  HSPW        128
-#define  HBPD         88
-#define  HOZVAL      799
-#define  HFPD         40
+#define HOZVAL 		(Horizontal_resolution-1)
+#define LINEVAL 	(Vertical_resolution-1)
 
+#define RightBotX   HOZVAL
+#define RightBotY   LINEVAL
+//#define LeftTopX     290
+//#define LeftTopY     260
 #define LeftTopX     0
 #define LeftTopY     0
 
-#define RightBotX   799
-#define RightBotY   599
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+#define  VSPW          9
+#define  VBPD         1
+#define  VFPD          1
+
+#define  HSPW        40
+#define  HBPD         1
+#define  HFPD         1
 
 #define FRAME_BUFFER   0x54000000
 
@@ -49,6 +76,8 @@ unsigned int ysize;
 
 void lcd_init(void)
 {
+
+	// GPIO CONFIG
 	GPICON = 0xaaaaaaaa;  /* set gpi0~gpi15 use for cd_vd[0~15] */
 	GPJCON = 0xaaaaaaa;   /* set gpj0~gpi11 use for LCD */
 
@@ -56,41 +85,83 @@ void lcd_init(void)
 	GPFCON |=  (1<<28);    /* GPF14 use as lcd on/off */
 
 
+	// sleep mode 的时候 输出 1
 	MIFPCON &= ~(1<<3);   /* Normal mode */
-	SPCON    &= ~(0x3);
-	SPCON    |= 0x1;            /* RGB I/F style */
+
+
+	if (DATA_INTERFACE_MODE == RGB){
+		// 最低两位 为 0b01
+		SPCON    &= ~(0x3);
+		SPCON    |= 0x1;            /* RGB I/F style */
+	}
+
+	// ~(3<<26)   // RGB interface
+	// ~(3<<17)   // RGB Parallel format (RGB)
+	// ~(3<<2) 	  // Video Clock source : HCLK
 
 	VIDCON0 &= ~((3<<26) | (3<<17) | (0xff<<6)  | (3<<2));     /* RGB I/F, RGB Parallel format,  */
-	VIDCON0 |= ((2<<6) | (1<<4) );      /* vclk== HCLK / (CLKVAL+1) = 133/15 = 9MHz */
+	// (2<<6) 	// VCLK = Video Clock source/3 , CLKVAL = 2
+	// (1<<4) 	// clock source  Divided by CLKVAL_F
+	/* vclk== HCLK / (CLKVAL+1) = 133/15 = 9MHz */ // ???
+	// vclk== HCLK / (CLKVAL+1) = 133/3 = 44.3MHz
+	VIDCON0 |= ((2<<6) | (1<<4) ); 
 
+	// Disable the video output and the Display control signal
 	VIDCON0 &= ~(0x3);
 
-	VIDCON1 &= ~(1<<7);   /*LCD driver gets the video data at VCLK falling edge */
-	VIDCON1 |= ((1<<6) | (1<<5));  /* HSYNC and VSYNC pulse polarity inverted */
 
+	/*LCD driver gets the video data at VCLK falling edge */
+	VIDCON1 &= ~(1<<7);
+	/* HSYNC and VSYNC pulse polarity inverted */
+	VIDCON1 |= ((1<<6) | (1<<5));
+
+	// Vertical back porch is the number of inactive lines at the start of a frame, after vertical synchronization period.
+	// Vertical front porch is the number of inactive lines at the end of a frame, before vertical synchronization period.
+	// Vertical sync pulse width determines the VSYNC pulse's high level width by counting the number of inactive lines.
 	VIDTCON0 = (VBPD << 16) | (VFPD << 8) | (VSPW << 0);
 	VIDTCON1 = (HBPD << 16) | (HFPD << 8) | (HSPW << 0);
+
+
+	// These bits determine the vertical size of display
+	// These bits determine the horizontal size of display
 	VIDTCON2 = (LINEVAL << 11) | (HOZVAL << 0);
 
-	WINCON0 &= ~(0xf << 2);
-	WINCON0 |= (0xb<<2);    /* unpacked 24 BPP (non-palletized R:8-G:8-B:8 ) */
+	// Select the BPP (Bits Per Pixel) mode Window image.
+	if (BitsPerPixel == 24){
+		WINCON0 &= ~(0xf << 2);
+		WINCON0 |= (0xb<<2);    /* unpacked 24 BPP (non-palletized R:8-G:8-B:8 ) */
+	}
 
+	// Horizontal screen coordinate for left top pixel of OSD image
+	/* Vertical screen coordinate for left top pixel of OSD image
+	   (for interlace TV output, this value MUST be set to half of the
+	   original screen y coordinate. And the original screen y coordinate
+	   MUST be even value.) */
 	VIDOSD0A = (LeftTopX<<11) | (LeftTopY << 0);
-	VIDOSD0B = ( 480 <<11) | ( 272 << 0);
-	VIDOSD0C = (LINEVAL + 1) * (HOZVAL + 1);
+	// Horizontal screen coordinate for right bottom pixel of OSD image
+	// Vertical screen coordinate for right bottom pixel of OSD image
+	VIDOSD0B = ( RightBotX <<11) | ( RightBotY << 0);
+	// Window Size
+	// Height * Width (Number of Word)
+	VIDOSD0C = (Vertical_resolution) * (Horizontal_resolution);
 
+	// Window 0’s buffer start address register, buffer 0
 	VIDW00ADD0B0 = FRAME_BUFFER;
-	VIDW00ADD1B0 =  (((HOZVAL + 1)*4 + 0) * (LINEVAL + 1)) & (0xffffff);
+	// Window 0’s buffer end address register, buffer 0
+	//VIDW00ADD1B0 =  ((Horizontal_resolution*4 + FRAME_BUFFER) * Vertical_resolution) & (0xffffff);
+	VIDW00ADD1B0 =  (FRAME_BUFFER+(Horizontal_resolution*4*Vertical_resolution)) & (0xffffff);
 	/* VBASEL = VBASEU + (LINEWIDTH+OFFSIZE) x (LINEVAL+1)
 	 *        = 0 + (480*4 + 0) * 272
 	 *        =
 	 */
-	VIDW00ADD2 =  HOZVAL + 1;
+	// Window 0’s buffer size register
+	// 不用设置
+	//VIDW00ADD2 =  Horizontal_resolution;
 
 	fb_base_addr = FRAME_BUFFER;
-	xsize = HOZVAL + 1;
-	ysize = LINEVAL + 1;
-	bpp   = 24;
+	xsize = Horizontal_resolution;
+	ysize = Vertical_resolution;
+	bpp   = BitsPerPixel;
 
 }
 
@@ -105,7 +176,9 @@ static void lcd_off(void)
 }
 void displaycon_on(void)
 {
+	// Enable the video output and the Display control signal
 	VIDCON0 |= 0x3;
+	// Enable the video output and the VIDEO control signal
 	WINCON0 |= 1;
 }
 
